4cc428867acc44b02d7115bee085dde7382aeec3
ishpiamazing <ishpiamazing@gmail.com>
2020-08-23 19:34:18 +0000

    module.exports added

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,15 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("✅✅✅  Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🔴🔴🔴  Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴🔴🔴  Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 
 
+module.exports = assertEqual;
