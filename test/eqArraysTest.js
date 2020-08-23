9aca2df01157ae9582368fb1f5bba23e88be8005
ishpiamazing <ishpiamazing@gmail.com>
2020-08-23 20:18:54 +0000

    eqArrays test added

---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays.js');
+const assertEqual = require('../assertEqual');
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
