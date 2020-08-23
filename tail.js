23652b0a139bb35f40dd0b6506908e181754329d
ishpiamazing <ishpiamazing@gmail.com>
2020-08-23 20:00:18 +0000

    module.exports added

--- tail.js
+++ tail.js
@@ -11,38 +11,24 @@ const tail = function(arr) {
 
 
 const assertEqual = function(actual, expected) {
-   
-  let tailStatus = true;
 
-  if ((actual.length) !== (expected.length)) {
-   
-    tailStatus = false;
-   
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      
-      if (actual[i] !== expected[i]) {
-        
-        tailStatus = false;
-        
-      }
-      
-    }
-
-  }
-  if (tailStatus) {
+  if (actual === expected) {
     console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else if (!tailStatus) {
+  } else {
     console.log(`🔴🔴🔴  Assertion Failed: ${actual} !== ${expected}`);
   }
- 
+
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+
+module.exports = tail;
+
+
+
+
+
+
 
 
