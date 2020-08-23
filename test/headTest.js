da58404d02c209982d6bfa665f2b4ec63633d48e
ishpiamazing <ishpiamazing@gmail.com>
2020-08-23 19:36:39 +0000

    headTest added

---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+
+
+const head = require('../head.js');
+const assertEqual = require('../assertEqual');
+
+
+ assertEqual(head([5,6,7]), 5);
+ assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+
