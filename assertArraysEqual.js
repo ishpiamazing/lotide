ed26b84314faf362bc294d9179266ed2ecfa0597
ishpiamazing <ishpiamazing@gmail.com>
2020-08-23 20:48:46 +0000

    module.exports added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,5 @@
-const eqArrays = function(arrOne, arrTwo) {
 
-  let value = true;
-
-  if (arrOne.length !== arrTwo.length) {
-    value = false;
-  } else {
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        value = false;
-        break;
-      } else {
-        value = true;
-      }
-    }
-  }
-  
-  return value;
-};
+const eqArrays = require('./eqArrays.js');
 
 
 const assertArraysEqual = function(actual, expected) {
@@ -30,7 +13,6 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1,2,4,5],[1,2,4,5]);
-assertArraysEqual([2,3,8],[2,2,8]);
-assertArraysEqual([3,4,5,6,7,8],[3,4,5,6,7,9]);
-assertArraysEqual(["Loopy","Lighthouse"],["Loopy","Lighthouse"]);
+
+
+module.exports = assertArraysEqual;
